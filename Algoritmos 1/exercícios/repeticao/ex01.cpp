c126a3b70a8d8fb89f16d0a686f1bc64727020a0
Paulo Cesar Jesuino Filho <110634982+Paulocjf@users.noreply.github.com>
2026-04-24 19:33:34 -0300

    Add repetition exercise to print messages

---
+++ Algoritmos 1/exercícios/repeticao/ex01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    
+    int contador = 1;
+    
+    while (contador <= 30) {
+        cout << contador << "\tNão vou colar na prova" << endl;
+        contador++;
+    }
+    
+    return 0;
+}
